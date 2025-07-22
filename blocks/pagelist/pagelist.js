3833b22c4b19afa71f139c8a11afa6c3400d5c50
shumpei.mishio <shumpei.mishio@ctc-g.co.jp>
2025-07-22 17:27:24 +0900

    mod pagelist block

---
+++ blocks/pagelist/pagelist.js
@@ -0,0 +1,7 @@
+export default function decorate(block) {
+  const div = document.createElement('div');
+  const p = document.createElement('p');
+  p.innerText('hogehogehoge')
+  div.append(p);
+  block.append(div);
+}
